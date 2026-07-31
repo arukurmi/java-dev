26fc388bb61e633dec8fa3215bdc811c9747c917
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:04 +0530

    decorator: build out HouseBlend (2/3)

--- src/Practice/StarbuzzCoffee/Beverages/HouseBlend.java
+++ src/Practice/StarbuzzCoffee/Beverages/HouseBlend.java
@@ -1,1 +1,9 @@
 package Practice.StarbuzzCoffee.Beverages;
+
+import Practice.StarbuzzCoffee.Beverage;
+import Practice.StarbuzzCoffee.Size;
+
+public class HouseBlend extends Beverage {
+    public String HouseBlendDescription = "This is a HouseBlend";
+    public double HouseBlendCost = 1.80;
+}
