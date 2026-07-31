b071e9d72f7523cf7d594003cfffa0bf98d57e74
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:03 +0530

    factory: finish NyStyleCheesePizza

--- src/Practice/factoryPattern/NyStylePizza/NyStyleCheesePizza.java
+++ src/Practice/factoryPattern/NyStylePizza/NyStyleCheesePizza.java
@@ -5,5 +5,10 @@ import Practice.factoryPattern.Pizza;
 public class NyStyleCheesePizza extends Pizza {
     public NyStyleCheesePizza(){
         name = "Ny style cheese pizza";
-}
+        dough = "Ny style dough";
+        sauce = "Ny style marinara sauce";
+        toppings.add("Onion sauce");
+        toppings.add("Grated mozz Cheese");
+        toppings.add("Grated blue Cheese");
+    }
 }
