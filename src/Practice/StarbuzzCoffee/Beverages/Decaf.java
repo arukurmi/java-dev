c0dfc464e30a5962a89909a5dc1f6bb4af751cbf
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:04 +0530

    decorator: build out Decaf (2/3)

--- src/Practice/StarbuzzCoffee/Beverages/Decaf.java
+++ src/Practice/StarbuzzCoffee/Beverages/Decaf.java
@@ -1,1 +1,9 @@
 package Practice.StarbuzzCoffee.Beverages;
+
+import Practice.StarbuzzCoffee.Beverage;
+import Practice.StarbuzzCoffee.Size;
+
+public class Decaf  extends Beverage {
+    public String DecafDescription = "This is a Decaf";
+    public double DecafCost = 1.40;
+}
