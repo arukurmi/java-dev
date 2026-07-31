5b6969272264964749e646a6438d6e6bc5ecfc3d
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:04 +0530

    decorator: finish HouseBlend

--- src/Practice/StarbuzzCoffee/Beverages/HouseBlend.java
+++ src/Practice/StarbuzzCoffee/Beverages/HouseBlend.java
@@ -6,4 +6,13 @@ import Practice.StarbuzzCoffee.Size;
 public class HouseBlend extends Beverage {
     public String HouseBlendDescription = "This is a HouseBlend";
     public double HouseBlendCost = 1.80;
+
+    public HouseBlend(Size size){
+        description = HouseBlendDescription;
+        setSize(size);
+    }
+
+    public double cost() {
+        return HouseBlendCost;
+    }
 }
