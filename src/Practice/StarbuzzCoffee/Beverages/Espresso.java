cde1bfc0c8647c3d9cbd54a00a2605bb2e60819e
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:04 +0530

    decorator: build out Espresso (2/3)

--- src/Practice/StarbuzzCoffee/Beverages/Espresso.java
+++ src/Practice/StarbuzzCoffee/Beverages/Espresso.java
@@ -1,1 +1,9 @@
 package Practice.StarbuzzCoffee.Beverages;
+
+import Practice.StarbuzzCoffee.Beverage;
+import Practice.StarbuzzCoffee.Size;
+
+public class Espresso extends Beverage {
+    public String EspressoDescription = "This is an Espresso";
+    public double EspressoCost = 1.40;
+}
