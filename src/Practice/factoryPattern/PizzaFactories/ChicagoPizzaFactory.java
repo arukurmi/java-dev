9c9abe182a81f044bdafb0285faa34fecdbe4e8d
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:04 +0530

    factory: build out ChicagoPizzaFactory (2/3)

--- src/Practice/factoryPattern/PizzaFactories/ChicagoPizzaFactory.java
+++ src/Practice/factoryPattern/PizzaFactories/ChicagoPizzaFactory.java
@@ -1,1 +1,11 @@
 package Practice.factoryPattern.PizzaFactories;
+
+import Practice.factoryPattern.ChicagoPizzas.ChicagoStyleCheesePizza;
+import Practice.factoryPattern.ChicagoPizzas.ChicagoVeggiePizza;
+import Practice.factoryPattern.Pizza;
+import Practice.factoryPattern.PizzaFactory;
+
+public class ChicagoPizzaFactory extends PizzaFactory {
+    public Pizza createPizza(String orderType){
+}
+}
