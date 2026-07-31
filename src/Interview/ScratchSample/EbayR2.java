41254627521efd2d8b35240656a210cb0f4af5f4
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:05 +0530

    practice: build out EbayR2 (2/7)

--- src/Interview/ScratchSample/EbayR2.java
+++ src/Interview/ScratchSample/EbayR2.java
@@ -1,1 +1,18 @@
 package Interview.ScratchSample;
+
+import java.util.*;
+
+public class EbayR2 {
+    /*
+     */
+    // Time: O(n)
+    // Space: O(n)
+    public static Map<String, Map<String, String>> convert(String[] input) {
+        List<String> sectionName = new ArrayList<>();
+        List<List<String[]>> sectionKeyValue = new ArrayList<>();
+        int currentSection = -1;
+
+        for (String s : input) {
+}
+}
+}
