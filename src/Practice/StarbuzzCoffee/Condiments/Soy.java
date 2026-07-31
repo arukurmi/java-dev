d462207145891aca357041f11a8cf50da0115ed8
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:05 +0530

    decorator: build out Soy (2/4)

--- src/Practice/StarbuzzCoffee/Condiments/Soy.java
+++ src/Practice/StarbuzzCoffee/Condiments/Soy.java
@@ -1,1 +1,9 @@
 package Practice.StarbuzzCoffee.Condiments;
+
+import Practice.StarbuzzCoffee.Beverage;
+import Practice.StarbuzzCoffee.CondimentsDecorator;
+import Practice.StarbuzzCoffee.Size;
+
+public class Soy extends CondimentsDecorator {
+    public double SoyCost = 0.10;
+}
