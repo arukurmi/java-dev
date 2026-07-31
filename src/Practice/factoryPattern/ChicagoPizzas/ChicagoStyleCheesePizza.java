823d0a55b0aabfc3920fef09f19cf226ce83fd21
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:03 +0530

    factory: build out ChicagoStyleCheesePizza (2/3)

--- src/Practice/factoryPattern/ChicagoPizzas/ChicagoStyleCheesePizza.java
+++ src/Practice/factoryPattern/ChicagoPizzas/ChicagoStyleCheesePizza.java
@@ -1,1 +1,11 @@
 package Practice.factoryPattern.ChicagoPizzas;
+
+import Practice.factoryPattern.Pizza;
+
+public class ChicagoStyleCheesePizza extends Pizza {
+    public ChicagoStyleCheesePizza(){
+        name = "Chicago style cheese pizza";
+        dough = "Chicago style dough";
+        sauce = "Chicago style marinara sauce";
+}
+}
