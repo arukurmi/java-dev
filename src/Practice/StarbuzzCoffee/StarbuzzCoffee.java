7d47faf8ad4daec91c046e8c8c299a3179c63e9e
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:05 +0530

    decorator: finish StarbuzzCoffee

--- src/Practice/StarbuzzCoffee/StarbuzzCoffee.java
+++ src/Practice/StarbuzzCoffee/StarbuzzCoffee.java
@@ -7,5 +7,10 @@ import Practice.StarbuzzCoffee.Condiments.Soy;
 
 public class StarbuzzCoffee {
     public static void main(String args[]){
-}
+        Beverage order = new Decaf(Size.VENTI);
+        Beverage condimentsDecorator = new Mocha(new Soy(order));
+        double cost = condimentsDecorator.cost();
+
+        System.out.print(condimentsDecorator.getDescription() + ", Costs: " + cost);
+    }
 }
