2cc91aa4bd97cf8d01f6fba6911bd48f0e8004ae
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:04 +0530

    factory: finish ChicagoPizzaFactory

--- src/Practice/factoryPattern/PizzaFactories/ChicagoPizzaFactory.java
+++ src/Practice/factoryPattern/PizzaFactories/ChicagoPizzaFactory.java
@@ -7,5 +7,10 @@ import Practice.factoryPattern.PizzaFactory;
 
 public class ChicagoPizzaFactory extends PizzaFactory {
     public Pizza createPizza(String orderType){
-}
+        return switch (orderType) {
+            case "cheese" -> new ChicagoStyleCheesePizza();
+            case "Veggie" -> new ChicagoVeggiePizza();
+            default -> new ChicagoStyleCheesePizza();
+        };
+    }
 }
