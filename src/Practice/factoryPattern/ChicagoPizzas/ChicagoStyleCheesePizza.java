168bd7294d4ceca154c66fe87eeccdbe7ee409a7
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:03 +0530

    factory: finish ChicagoStyleCheesePizza

--- src/Practice/factoryPattern/ChicagoPizzas/ChicagoStyleCheesePizza.java
+++ src/Practice/factoryPattern/ChicagoPizzas/ChicagoStyleCheesePizza.java
@@ -7,5 +7,11 @@ public class ChicagoStyleCheesePizza extends Pizza {
         name = "Chicago style cheese pizza";
         dough = "Chicago style dough";
         sauce = "Chicago style marinara sauce";
-}
+        toppings.add("Grated Reggiano Cheese");
+        toppings.add("Grated Standard Cheese");
+    }
+
+    public void cut(){
+        System.out.println("Cutting pizza in square");
+    }
 }
