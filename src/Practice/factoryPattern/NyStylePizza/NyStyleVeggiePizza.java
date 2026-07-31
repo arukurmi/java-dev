cce2f1c3fa79e2ab3457610a32f33300c8b8b058
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:03 +0530

    factory: build out NyStyleVeggiePizza (2/3)

--- src/Practice/factoryPattern/NyStylePizza/NyStyleVeggiePizza.java
+++ src/Practice/factoryPattern/NyStylePizza/NyStyleVeggiePizza.java
@@ -1,1 +1,10 @@
 package Practice.factoryPattern.NyStylePizza;
+
+import Practice.factoryPattern.Pizza;
+
+public class NyStyleVeggiePizza extends Pizza {
+    public NyStyleVeggiePizza(){
+        name = "Ny style veggie pizza";
+        dough = "Ny style dough";
+}
+}
