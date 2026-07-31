88104697aee82a5bd0bc224a278a035b1d8befb0
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:04 +0530

    decorator: build out Beverage (2/3)

--- src/Practice/StarbuzzCoffee/Beverage.java
+++ src/Practice/StarbuzzCoffee/Beverage.java
@@ -1,1 +1,11 @@
 package Practice.StarbuzzCoffee;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public abstract class Beverage {
+    public String description = "This is some description";
+    @Getter
+    @Setter
+    public Size size;
+}
