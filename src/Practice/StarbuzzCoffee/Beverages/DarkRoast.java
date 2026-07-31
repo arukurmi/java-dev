5968ae4ef4c5df57363b8beb3e0025d28a4472d3
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:04 +0530

    decorator: finish DarkRoast

--- src/Practice/StarbuzzCoffee/Beverages/DarkRoast.java
+++ src/Practice/StarbuzzCoffee/Beverages/DarkRoast.java
@@ -6,4 +6,13 @@ import Practice.StarbuzzCoffee.Size;
 public class DarkRoast extends Beverage {
     public String DarkRoastDescription = "This is a DarkRoast";
     public double DarkRoastCost = 1.80;
+
+    public DarkRoast(Size size){
+        description = DarkRoastDescription;
+        setSize(size);
+    }
+
+    public double cost() {
+        return DarkRoastCost;
+    }
 }
