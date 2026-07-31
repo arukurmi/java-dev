62eeaeca47bbb11864c13bc0cbbf049021290ab3
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:04 +0530

    decorator: build out DarkRoast (2/3)

--- src/Practice/StarbuzzCoffee/Beverages/DarkRoast.java
+++ src/Practice/StarbuzzCoffee/Beverages/DarkRoast.java
@@ -1,1 +1,9 @@
 package Practice.StarbuzzCoffee.Beverages;
+
+import Practice.StarbuzzCoffee.Beverage;
+import Practice.StarbuzzCoffee.Size;
+
+public class DarkRoast extends Beverage {
+    public String DarkRoastDescription = "This is a DarkRoast";
+    public double DarkRoastCost = 1.80;
+}
