4b3c39d7ae3e8f96a35eed3fada44f3ff422ee4e
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:04 +0530

    decorator: build out Mocha (2/4)

--- src/Practice/StarbuzzCoffee/Condiments/Mocha.java
+++ src/Practice/StarbuzzCoffee/Condiments/Mocha.java
@@ -1,1 +1,11 @@
 package Practice.StarbuzzCoffee.Condiments;
+
+import Practice.StarbuzzCoffee.Beverage;
+import Practice.StarbuzzCoffee.CondimentsDecorator;
+import Practice.StarbuzzCoffee.Size;
+
+public class Mocha extends CondimentsDecorator {
+    public double MochaCost = 0.40;
+    public Mocha(Beverage beverage){
+}
+}
