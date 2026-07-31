ddfa9f95e67fd043e39e75836785592af75620f6
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:04 +0530

    decorator: finish Decaf

--- src/Practice/StarbuzzCoffee/Beverages/Decaf.java
+++ src/Practice/StarbuzzCoffee/Beverages/Decaf.java
@@ -6,4 +6,13 @@ import Practice.StarbuzzCoffee.Size;
 public class Decaf  extends Beverage {
     public String DecafDescription = "This is a Decaf";
     public double DecafCost = 1.40;
+
+    public Decaf(Size size){
+        description = DecafDescription;
+        setSize(size);
+    }
+
+    public double cost() {
+        return DecafCost;
+    }
 }
