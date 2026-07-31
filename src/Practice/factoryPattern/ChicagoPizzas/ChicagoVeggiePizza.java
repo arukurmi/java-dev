af88eb60015a802ef6a4ab863832cad3b9a0b4ac
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:03 +0530

    factory: build out ChicagoVeggiePizza (2/4)

--- src/Practice/factoryPattern/ChicagoPizzas/ChicagoVeggiePizza.java
+++ src/Practice/factoryPattern/ChicagoPizzas/ChicagoVeggiePizza.java
@@ -1,1 +1,9 @@
 package Practice.factoryPattern.ChicagoPizzas;
+
+import Practice.factoryPattern.Pizza;
+
+public class ChicagoVeggiePizza extends Pizza {
+    public ChicagoVeggiePizza(){
+        name = "Chicago style veggie pizza";
+}
+}
