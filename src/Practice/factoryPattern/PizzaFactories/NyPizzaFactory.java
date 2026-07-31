e00c1442c9143a65904693d9dc67fadeaba0b2d6
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:04 +0530

    factory: build out NyPizzaFactory (2/3)

--- src/Practice/factoryPattern/PizzaFactories/NyPizzaFactory.java
+++ src/Practice/factoryPattern/PizzaFactories/NyPizzaFactory.java
@@ -1,1 +1,11 @@
 package Practice.factoryPattern.PizzaFactories;
+
+import Practice.factoryPattern.NyStylePizza.NyStyleCheesePizza;
+import Practice.factoryPattern.NyStylePizza.NyStyleVeggiePizza;
+import Practice.factoryPattern.Pizza;
+import Practice.factoryPattern.PizzaFactory;
+
+public class NyPizzaFactory extends PizzaFactory {
+    public Pizza createPizza(String orderType){
+}
+}
