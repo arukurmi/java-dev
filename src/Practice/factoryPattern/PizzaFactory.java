8deb14f517e28c9f2a0d4304ce5f2b460b800e39
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:03 +0530

    factory: build out PizzaFactory (2/3)

--- src/Practice/factoryPattern/PizzaFactory.java
+++ src/Practice/factoryPattern/PizzaFactory.java
@@ -1,1 +1,9 @@
 package Practice.factoryPattern;
+
+public abstract class PizzaFactory {
+    public Pizza orderPizza(String orderType){
+        Pizza pizza = createPizza(orderType);
+        pizza.prepare();
+        pizza.bake();
+}
+}
