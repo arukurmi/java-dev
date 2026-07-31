990c6911205ea8ba637c350d9b28046a4da1ae8e
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:05 +0530

    decorator: build out WhipCream (2/4)

--- src/Practice/StarbuzzCoffee/Condiments/WhipCream.java
+++ src/Practice/StarbuzzCoffee/Condiments/WhipCream.java
@@ -1,1 +1,9 @@
 package Practice.StarbuzzCoffee.Condiments;
+
+import Practice.StarbuzzCoffee.Beverage;
+import Practice.StarbuzzCoffee.CondimentsDecorator;
+import Practice.StarbuzzCoffee.Size;
+
+public class WhipCream extends CondimentsDecorator {
+    public double WhipCreamCost = 0.20;
+}
