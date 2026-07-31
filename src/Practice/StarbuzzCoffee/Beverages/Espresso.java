43b6d5947da0328ea76e087a576cbeb08bde377a
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:04 +0530

    decorator: finish Espresso

--- src/Practice/StarbuzzCoffee/Beverages/Espresso.java
+++ src/Practice/StarbuzzCoffee/Beverages/Espresso.java
@@ -6,4 +6,13 @@ import Practice.StarbuzzCoffee.Size;
 public class Espresso extends Beverage {
     public String EspressoDescription = "This is an Espresso";
     public double EspressoCost = 1.40;
+
+    public Espresso(Size size){
+        description = EspressoDescription;
+        setSize(size);
+    }
+
+    public double cost() {
+        return EspressoCost;
+    }
 }
