0dce383154884320a916a5ec1f202c722b55eefc
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:05 +0530

    practice: build out GreenStitchR1 (2/6)

--- src/Interview/ScratchSample/GreenStitchR1.java
+++ src/Interview/ScratchSample/GreenStitchR1.java
@@ -1,1 +1,21 @@
 package Interview.ScratchSample;
+
+public class GreenStitchR1 {
+    /*
+        Input:
+        foorgeeksskeegfor
+        Output:
+        geeksskeeg
+        Input:
+        BBABCBAB
+        Output:
+        BABCBAB
+     */
+
+    // Time: O(n^2)
+    // Space: O(1)
+    public static String findLongestPalindromicSubstring(String inputString) {
+        String longestPalindrome = "";
+        int lenthOfInputString = inputString.length();
+}
+}
