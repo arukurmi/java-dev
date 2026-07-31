4a626e9e58291fa34314e8a6ad20f256849ffa99
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:04 +0530

    decorator: finish CondimentsDecorator

--- src/Practice/StarbuzzCoffee/CondimentsDecorator.java
+++ src/Practice/StarbuzzCoffee/CondimentsDecorator.java
@@ -3,4 +3,9 @@ package Practice.StarbuzzCoffee;
 public abstract class CondimentsDecorator extends Beverage{
     public Beverage beverage;
     public abstract double cost();
+
+    @Override
+    public Size getSize() {
+        return beverage.getSize();
+    }
 }
