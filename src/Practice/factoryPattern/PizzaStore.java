2b92a97b2d7db8134bb1bcfda0486c7cafe79bdb
Aryansh Kurmi <arukurmi22@gmail.com>
2026-07-31 23:02:04 +0530

    factory: finish PizzaStore

--- src/Practice/factoryPattern/PizzaStore.java
+++ src/Practice/factoryPattern/PizzaStore.java
@@ -5,4 +5,10 @@ import Practice.factoryPattern.PizzaFactories.ChicagoPizzaFactory;
 import Practice.factoryPattern.PizzaFactories.NyPizzaFactory;
 
 public class PizzaStore {
+    public static void main(String args[]){
+        PizzaFactory nyPizzaStore = new NyPizzaFactory();
+        PizzaFactory chicagoPizzaStore = new ChicagoPizzaFactory();
+        Pizza pizza = nyPizzaStore.orderPizza("cheese");
+        Pizza pizza2 = chicagoPizzaStore.orderPizza("Veggie");
+    }
 }
