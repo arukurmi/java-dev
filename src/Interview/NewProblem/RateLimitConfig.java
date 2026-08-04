0a9f739596f02b0fbf66b6eebf316c1153792be9
Aryansh Kurmi <arukurmi22@gmail.com>
2026-08-04 23:26:21 +0530

    add file

--- src/Interview/NewProblem/RateLimitConfig.java
+++ src/Interview/NewProblem/RateLimitConfig.java
@@ -1,4 +1,34 @@
 package Interview.NewProblem;
 
+import Interview.NewProblem.Strategy.Strategy;
+import lombok.Getter;
+import lombok.Setter;
+
 public class RateLimitConfig {
+    @Getter
+    @Setter
+    private long maxRequests;
+    @Getter
+    @Setter
+    private long windowMillis;
+    @Getter
+    @Setter
+    private Strategy strategy;
+    @Getter
+    @Setter
+    private long capacity;
+
+    private RateLimitConfig(){
+
+    }
+
+    static class RateLimitBuilder{
+        Strategy strategy = Strategy.FIXED_WINDOW;
+        long maxRequests = 100;
+        long windowMillis = 10000;
+        long capacity = 50;
+
+
+    }
+
 }
